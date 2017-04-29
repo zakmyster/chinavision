3aaa2a4e279b1cc08e4d6405d7224d924408ca3b
Zak Sayed <zaksayed@Zaks-MacBook-Air.local>
2017-04-29 18:04:03 +1000

    reshuffle workspace directory structure with component folder; register workspace module with app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { WorkspaceComponent } from './workspace/workspace.component';
+import { WorkspaceComponent } from './workspace/components/workspace/workspace.component';
+import { WorkspaceModule } from 'app/workspace/workspace.module';
 
 @NgModule({
-  declarations: [
-    WorkspaceComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WorkspaceModule
   ],
   providers: [],
   bootstrap: [WorkspaceComponent]
--- src/app/workspace/workspace.module.ts
+++ src/app/workspace/workspace.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WorkspaceRoutingModule } from './workspace-routing.module';
-import { WorkspaceComponent } from './workspace.component';
-import { WorkspaceTopNavComponent } from './workspace-top-nav/workspace-top-nav.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { WorkspaceTopNavComponent } from './components/workspace-top-nav/workspace-top-nav.component';
 
 @NgModule({
   imports: [
     CommonModule,
     WorkspaceRoutingModule
   ],
-  declarations: [WorkspaceComponent, WorkspaceTopNavComponent]
+  declarations: [
+    WorkspaceComponent,
+    WorkspaceTopNavComponent
+  ]
 })
 export class WorkspaceModule { }
